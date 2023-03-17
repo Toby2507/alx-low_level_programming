9feb758a4ea83cede67b0ec671467ea7e9f56b85
Toby2507 <oluwatobisalau2000@gmail.com>
2023-03-17 22:26:01 +0000

    Add a C file that prints all possible different combinations of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of 3 digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if ((i < j) && (j < k))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
