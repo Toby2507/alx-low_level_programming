f449e5fa19f74a9eeec7575812451016a58f3568
Toby2507 <oluwatobisalau2000@gmail.com>
2023-03-21 21:46:31 +0000

    Refactor code on file 9 to produce the specified format

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,19 @@ void times_table(void)
 			short x2 = x % 10;
 
 			if (x1 > 0)
+			{
 				_putchar(x1 + '0');
+			}
+			else
+			{
+				if (j != 0)
+					_putchar(' ');
+			}
 			_putchar(x2 + '0');
-			if (j != 9)
+			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (x < 10 && x1 == 0)
-					_putchar(' ');
 			}
 			else
 			{
