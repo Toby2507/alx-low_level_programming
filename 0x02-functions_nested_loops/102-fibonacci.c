327b3de827e951feef5bb2a594a9a95d4bf5907f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-03-21 23:21:51 +0000

    Add a C file that prints the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * fib - Computes the first 50 Fibonacci numbers
+ * @n: The nth number in the fib sequence
+ *
+ * Return: All 50 computed fib numbers
+ */
+long long int fib(int n)
+{
+	static long long int memo[50] = {0};
+
+	if (n == 1 || n == 2)
+		return (1);
+	if (memo[n])
+		return (memo[n]);
+	memo[n] = fib(n - 2) + fib(n - 1);
+	return (memo[n]);
+}
+
+/**
+ * main - Prints first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	short i;
+
+	for (i = 1; i <= 50; i++)
+		if (i < 50)
+			printf("%lld, ", fib(i));
+		else
+			printf("%lld", fib(i));
+	printf("\n");
+	return (0);
+}
+
