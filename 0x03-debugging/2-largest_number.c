dfd6dfdc48281c4dc45ccfa4be96af277dcca686
Toby2507 <oluwatobisalau2000@gmail.com>
2023-03-22 13:45:35 +0000

    Update conditional in file 2 to accomodate all problem senerios

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,10 +9,10 @@
  */
 int largest_number(int a, int b, int c)
 {
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 		return (a);
-	else if (b > a && b > c)
+	if (b >= a && b >= c)
 		return (b);
-	else
-		return (c);
+	return (c);
 }
+
