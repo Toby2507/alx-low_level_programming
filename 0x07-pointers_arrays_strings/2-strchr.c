d95542857c256a252ef84ed31cec33e4051977f9
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-03 13:33:24 +0000

    Refactor function _strchr to remove the stddef header file using '\0' instead of NULL

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - Locates a character in a string
@@ -16,5 +15,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }
