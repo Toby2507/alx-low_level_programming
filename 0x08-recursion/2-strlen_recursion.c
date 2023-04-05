c4faa74ef79956031df0e3413d1d441c8ed611a3
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-05 09:32:50 +0000

    Add a function the computes the length of a string and returns it

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - computes the length of a string
+ * @s: string whose length is to be computed
+ *
+ * Return: length of string @s
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
