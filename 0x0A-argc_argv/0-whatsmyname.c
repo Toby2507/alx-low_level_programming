7ded1e5b2e5b07c6a571663a52d66a363ea20c65
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-06 14:26:01 +0000

    Add a program that prints its own saved name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
