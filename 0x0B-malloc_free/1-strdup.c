70452fd8901b6ac3c8c06c64e987a494aaa7c176
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-11 17:59:47 +0000

    Add a function that copies a string into a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - allocate space in memory and copy some strings into it
+ * @str: string to be copied into memory
+ *
+ * Return: pointer to the created memory
+ */
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *str2 = NULL;
+
+	while (str[len])
+		len++;
+	if (len == 0)
+		return (NULL);
+	str2 = malloc((len * sizeof(char)));
+	if (!str2)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		str2[i] = str[i];
+	str2[i] = '\0';
+	return (str2);
+}
