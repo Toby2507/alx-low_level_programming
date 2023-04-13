0a502aa3145616963f4764033156364358e24669
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-13 14:34:40 +0000

    Update _calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	void *arr;
 
-	if (nmemb < 1 || size < 1)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (!arr)
