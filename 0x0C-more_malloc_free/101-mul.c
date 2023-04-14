514891a55c502aed85644c7392b34120cd1f4dd0
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-14 00:01:59 +0000

    Refactor file 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,8 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char);
 int _strlen(char *);
-char *_realloc(char *, unsigned int);
-void strrev(char *, int);
-char *stradd(char *, char *);
-char *strmul(char *, char, int, int);
+char *strmul(char *, char *);
 void print(char *);
 void err(void);
 
@@ -19,25 +15,12 @@ void err(void);
  */
 int main(int argc, char **argv)
 {
-	int len1, len2, i, j = 0;
-	char *s1, *s2, *res = malloc(1);
+	char *res;
 
-	if (argc != 3)
+	if (argc != 3 || !_strlen(argv[1]) || !_strlen(argv[2]))
 		err();
-	s1 = argv[1];
-	s2 = argv[2];
-	*res = '\0';
-	len1 = _strlen(s1) - 1;
-	len2 = _strlen(s2) - 1;
-	for (i = len2; i >= 0; i--)
-		res = stradd(res, strmul(s1, s2[i], len1, j++));
-	if (!atoi(res))
-	{
-		_putchar('0');
-		_putchar('\n');
-	}
-	else
-		print(res);
+	res = strmul(argv[1], argv[2]);
+	print(res);
 	free(res);
 	return (0);
 }
@@ -55,131 +38,64 @@ int _strlen(char *s)
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-			err();
+			return (0);
 		i++;
 	}
 	return (i);
 }
 
 /**
- * _realloc - increase the size of a memory block
- * @ptr: memory block
- * @size: new size
- *
- * Return: pointer to the new memory block
- */
-char *_realloc(char *ptr, unsigned int size)
-{
-	char *newptr;
-	unsigned int i;
-
-	newptr = malloc(size);
-	if (!newptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	for (i = 0; i < size - 1; i++)
-		newptr[i] = ptr[i];
-	free(ptr);
-	return (newptr);
-}
-
-/**
- * strrev - reverses a string
- * @s: string
- * @n: size of string
- */
-void strrev(char *s, int n)
-{
-	int i;
-
-	for (i = 0; i <= n / 2; i++)
-	{
-		char temp = s[i];
-
-		s[i] = s[n - i];
-		s[n - i] = temp;
-	}
-}
-
-/**
- * stradd - adds 2 strings together
+ * strmul - multiply 2 strings
  * @s1: string 1
  * @s2: string 2
  *
  * Return: pointer to the result
  */
-char *stradd(char *s1, char *s2)
+char *strmul(char *s1, char *s2)
 {
-	char *res;
-	int carry = 0, i = 0, n1 = _strlen(s1), n2 = _strlen(s2);
+	int len1 = _strlen(s1), len2 = _strlen(s2), size, i, j;
+	char *res, *temp;
 
-	res = malloc(i + 1);
+	size = len1 + len2;
+	res = malloc(sizeof(char) * (size + 1));
 	if (!res)
-	{
-		free(s1);
-		free(s2);
 		err();
-	}
-	while (n1 > 0 || n2 > 0 || carry)
+	for (i = 0; i < size; i++)
+		res[i] = '0';
+	res[size] = '\0';
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		int sum = carry;
+		int carry = 0;
 
-		res = _realloc(res, i + 2);
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int prod = (s1[i] - '0') * (s2[j] - '0');
+
+			prod += (res[i + j + 1] - '0') + carry;
+			res[i + j + 1] = prod % 10 + '0';
+			carry = prod / 10;
+		}
+		res[i] += carry;
+	}
+	temp = res;
+	while (*temp == '0' && *(temp + 1))
+		temp++;
+	if (temp != res)
+	{
+		int diff = temp - res;
+
+		size -= diff;
+		res = malloc(sizeof(char) * (size + 1));
 		if (!res)
 		{
-			free(s1);
-			free(s2);
+			free(temp - diff);
 			err();
 		}
-		if (n1 > 0)
-			sum += s1[--n1] - '0';
-		if (n2 > 0)
-			sum += s2[--n2] - '0';
-		carry = sum / 10;
-		res[i++] = sum % 10 + '0';
+		for (i = 0; i < size; i++)
+			res[i] = temp[i];
+		res[size] = '\0';
+		free(temp - diff);
 	}
-	res[i] = '\0';
-	strrev(res, i - 1);
-	free(s1);
-	free(s2);
-	return (res);
-}
-
-/**
- * strmul - multiply a string by a character
- * @s: string
- * @c: character
- * @n: size of @s
- * @pad: no of prefix 0 padding
- *
- * Return: pointer to the result
- */
-char *strmul(char *s, char c, int n, int pad)
-{
-	char *res;
-	int carry = 0, i = 0, j = 0;
-
-	res = malloc(pad + 1);
-	if (!res)
-		err();
-	for (i = 0; i < pad; i++)
-		res[i] = '0';
-	while (n >= 0 || carry)
-	{
-		int mul = carry;
-
-		res = _realloc(res, pad + j + 2);
-		if (!res)
-			err();
-		if (n >= 0)
-			mul += (s[n--] - '0') * (c - '0');
-		carry = mul / 10;
-		res[pad + j++] = mul % 10 + '0';
-	}
-	res[pad + j] = '\0';
-	strrev(res, pad + j - 1);
 	return (res);
 }
 
