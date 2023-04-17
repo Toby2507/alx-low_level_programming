0a63f37258895347c619ebb4dd3d1b3a5ca2ab29
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 21:14:02 +0000

    Add a typedef to struct dog in dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dog {
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
