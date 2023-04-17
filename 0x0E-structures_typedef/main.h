6408232b8672ebf1372e0f2249bf1f647228c52f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 20:48:19 +0000

    Add main header file for pwd

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *);
+dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
+
+#endif /* MAIN_H */
