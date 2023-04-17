7789cb37c60f5538720a6fd9e9ac7c73e373d0ca
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 23:15:55 +0000

    Update new_dog to do a check if name and owner is defined

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(dog_t));
-	if (!d)
-		return (NULL);
-	d->name = strdup(name);
-	d->age = age;
-	d->owner = strdup(owner);
+	if (!name && !owner)
+	{
+		d = malloc(sizeof(dog_t));
+		if (!d)
+			return (NULL);
+		d->name = strdup(name);
+		d->age = age;
+		d->owner = strdup(owner);
+	}
 	return (d);
 }
