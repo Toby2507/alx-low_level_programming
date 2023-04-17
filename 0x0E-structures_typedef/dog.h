dd8e941d941a29775344936cba9aa67771fe79bc
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 21:37:22 +0000

    Remove the typedef and declarations of dog_t in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,15 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct dog {
+struct dog {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
-dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *);
 
 #endif /* MAIN_H */
