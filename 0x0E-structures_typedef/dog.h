d3200f9b2efe6ac845e237bb76486c59d0da0b0f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 21:46:52 +0000

    Update dog.h with the appropriate documentation for the structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,14 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dog {
+/**
+ * struct dog - dog prototype
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: This struct creates a dog with it's name, age and owner details
+ * recorded
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
+dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *);
 
 #endif /* MAIN_H */
