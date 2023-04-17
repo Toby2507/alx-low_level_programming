bd9b22e240dee2b8705dab36ebf9cc60e89ca0db
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-17 21:09:28 +0000

    Update dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
