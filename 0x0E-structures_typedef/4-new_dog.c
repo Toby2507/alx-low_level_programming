f40ed2637596997b66c2449ff8a9459f2b171c5a
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-18 00:05:54 +0000

    Update new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,23 +12,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *tmpname, *tmpowner;
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	d->name = strdup(name);
-	if (!d->name)
+	tmpname = strdup(name);
+	if (!tmpname)
 	{
 		free(d);
 		return (NULL);
 	}
-	d->owner = strdup(owner);
-	if (!d->owner)
+	d->name = tmpname;
+	tmpowner = strdup(owner);
+	if (!tmpowner)
 	{
-		free(d->name);
+		free(tmpname);
 		free(d);
 		return (NULL);
 	}
+	d->owner = tmpowner;
 	d->age = age;
 	return (d);
 }
