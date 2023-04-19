623c234fec15b43bac67e5bfab220985cc6ff3a5
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-19 22:27:51 +0000

    Add function pointers header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void print_name(char *, void (*f)(char *));
+void array_iterator(int *, size_t, void (*action)(int));
+int int_index(int *, int, int (*cmp)(int));
+
+#endif /* MAIN_H */
