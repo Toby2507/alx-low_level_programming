7d96d9a1fa06c8e3d276213adb9b02d351decf5e
Toby2507 <oluwatobisalau2000@gmail.com>
2023-05-01 12:14:03 +0000

    Refactor print_listint_safe to use floyd's cycle detection algorithm making it more efficient and accurate

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,19 +9,32 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *temp;
+	const listint_t *slow = head, *fast = head;
 
-	while (head)
+	while (slow && fast && fast->next)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
-		temp = head;
-		head = head->next;
-		if (temp <= head)
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			return (98);
+			slow = head;
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+				printf("[%p] %d\n", (void *)fast, fast->n);
+				fast = fast->next;
+			}
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			return (count + 1);
 		}
+		count++;
+	}
+	for (; slow; slow = slow->next)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
 	}
 	return (count);
 }
