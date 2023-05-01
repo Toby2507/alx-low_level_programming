b25e2f75e32ad8dcb062e15d794b017cd986be68
Toby2507 <oluwatobisalau2000@gmail.com>
2023-05-01 10:40:30 +0000

    Add a function that frees the allocated memory for a linked list including cyclic list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free a linked list
+ * @h: head of the list
+ *
+ * Return: size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *curr, *next;
+	size_t bytes = 0;
+
+	if (!h || !(*h))
+		return (bytes);
+	curr = *h;
+	while (curr)
+	{
+		next = curr->next;
+		free(curr);
+		bytes += sizeof(listint_t);
+		if (next >= curr)
+		{
+			*h = NULL;
+			return (bytes);
+		}
+		curr = next;
+	}
+	*h = NULL;
+	return (bytes);
+}
