d79b31b474df4d4674f61f63f5fd4961f42840ba
Toby2507 <oluwatobisalau2000@gmail.com>
2023-05-01 09:27:46 +0000

    Add a function that prints a linked list even if it's cyclic

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list
+ * @head: head of list
+ *
+ * Return: no of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *temp;
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		temp = head;
+		head = head->next;
+		if (temp <= head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+	}
+	return (count);
+}
