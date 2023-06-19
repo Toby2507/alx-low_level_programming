8bbab49c4e533c6b2b13a61ee86a582af323ee1f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-19 21:20:08 +0000

    Add a shell script that alters the output of a program

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/othma125/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+https://github.com/Toby2507/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
