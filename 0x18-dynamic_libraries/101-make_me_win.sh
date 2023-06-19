f259b958c22f920c520d4082e464a9b10ee5dad3
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-19 21:44:19 +0000

    Update download link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-https://github.com/Toby2507/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/Toby2507/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
