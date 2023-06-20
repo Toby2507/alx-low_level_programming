87b667169e024fa8bce3fd2cd58b47d5669b8e59
Toby Salau <oluwatobisalau2000@gmail.com>
2023-06-19 18:50:53 -0700

    Update the make me win fike

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Toby2507/alx-low_level_programming/blob/11efce568cb34b8ddefd973d01c01f0c73848b66/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/othma125/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
