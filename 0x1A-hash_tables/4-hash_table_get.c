040543d5a21e722a5b9b80bdefeea1f4e9305a9f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-07 22:26:37 +0100

    Add invalid input check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,9 +9,13 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long idx = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *item = ht->array[idx];
+	unsigned long idx;
+	hash_node_t *item;
 
+	if (key == NULL || ht == NULL || *key == '\0')
+		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[idx];
 	for (; item; item = item->next)
 		if (strcmp(item->key, key) == 0)
 			return (item->value);
