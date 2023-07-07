a25f4ad09644c2b53f46a36117d4e6398e7f0fdd
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-07 21:47:31 +0100

    Add error check for malloc error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long i;
 
+	if (!table)
+		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (!table->array)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);
