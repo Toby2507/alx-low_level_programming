3ca4b5dc5725293f50b853a37904a86854282ed5
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-07 18:10:31 +0100

    Fix betty no return doc error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - creates a hash table
  * @size: size of the hash table
  *
- * Returns: a pointer to the created hash table
+ * Return: a pointer to the created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
